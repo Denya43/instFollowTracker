c9430117c959e136ca709e763126493e22c7daea
Denis Denis <gdenia1@gmail.com>
2020-04-09 01:20:56 +0300

    Initial Commit

---
+++ intFollowTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  intFollowTracker
+//
+//  Created by Денис Гареев on 09.04.2020.
+//  Copyright © 2020 Denis Denis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
